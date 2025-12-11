59db152ebb7ee58b02e4dc8f95589fee735efcaa
shivansh kuntal <125203750+ShivanshKuntal@users.noreply.github.com>
2025-12-11 22:05:33 +0530

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,12 @@
+import java.io.*;
+ 
+class Main {
+    public static int liner(int arr[], int x)
+    {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x)
+                return i;
+        }
+        return -1;
+    }
+}
